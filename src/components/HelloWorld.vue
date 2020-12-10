f149daf22bd7dc58f9653ab690cc360d135c2daf
朱东东 <429299291@qq.com>
2020-12-10 16:23:18 +0800

    'lititong'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,54 @@
 <template>
-<div class="main">
-  <div id='stars'></div>
-  <div id='stars2'></div>
-  <div id='stars3'></div>
-  <div id='title'>
-    <span>
-      HELLO WORLD
-    </span>
-    <br>
-    <span class="textmain">
-      The more you see, the more you understand humility and admiration
-    </span>
+  <div class="lititong">
+  <header>
+    <div class="logo"><svg t="1607581779546" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6489" width="34" height="34"><path d="M641.28 278.613333l-45.226667-45.226666-278.634666 278.762666 278.613333 278.485334 45.248-45.269334-233.365333-233.237333z" p-id="6490" fill="#ffffff"></path></svg></div>
+    <h5 class="title">立体通应用商城</h5>
+    <div class="menu"><svg t="1607582144306" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="6920" width="34" height="34"><path d="M469.333 192c153.174 0 277.334 124.16 277.334 277.333 0 68.054-24.534 130.411-65.216 178.688L846.336 818.24l-48.341 49.877L630.4 695.125a276.053 276.053 0 0 1-161.067 51.542C316.16 746.667 192 622.507 192 469.333S316.16 192 469.333 192z m0 64C351.51 256 256 351.51 256 469.333s95.51 213.334 213.333 213.334 213.334-95.51 213.334-213.334S587.157 256 469.333 256z" p-id="6921" fill="#ffffff"></path></svg></div>
+  </header>
+  <div class="content">
+    <h4 class="model_title">3D智慧膜贴膜教程</h4>
+    <div class="swiper-container">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide"><video src="https://web-personal-zhu.oss-cn-shenzhen.aliyuncs.com/video/%E5%A4%A7mp4" poster="./poster.jpg" muted controls></video>
+          <h5>立体通超神啦</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, ex odit expedita sunt sit alias ab ratione ipsam doloribus sapiente, iusto dignissimos, placeat tenetur. Dolor inventore praesentium beatae aliquid harum!</p></div>
+        <div class="swiper-slide"><video src="https://web-personal-zhu.oss-cn-shenzhen.aliyuncs.com/video/%E5%A4%A7mp4" poster="./poster.jpg" muted controls></video>
+          <h5>立体通超神啦222</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores tempora recusandae, consequatur fuga laborum est tenetur velit veniam. Corporis quos sunt molestias eius consequatur quidem adipisci doloribus, dolorem a repudiandae.</p></div>
+        <div class="swiper-slide"><video src="https://web-personal-zhu.oss-cn-shenzhen.aliyuncs.com/video/%E5%A4%A7mp4" poster="./poster.jpg" muted controls></video>
+          <h5>立体通超神啦333</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat doloribus suscipit mollitia perspiciatis commodi possimus pariatur quo magni beatae soluta. Dolorem iste voluptate quisquam recusandae inventore adipisci delectus id possimus?</p></div>
+        <div class="swiper-slide"><video src="https://web-personal-zhu.oss-cn-shenzhen.aliyuncs.com/video/%E5%A4%A7mp4" poster="./poster.jpg" muted controls></video>
+          <h5>立体通超神啦444</h5>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis numquam libero quis illo enim maiores, expedita cumque iusto explicabo nihil molestiae repudiandae aliquid. Et quod nesciunt incidunt placeat voluptate recusandae.</p></div>
+        <div class="swiper-slide"><video src="https://web-personal-zhu.oss-cn-shenzhen.aliyuncs.com/video/%E5%A4%A7mp4" poster="./poster.jpg" muted controls></video>
+          <h5>立体通超深啦</h5>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis architecto, iusto iure laborum excepturi, temporibus, expedita libero vitae obcaecati quos corrupti pariatur! Nam quas maiores quia veniam delectus incidunt minima?</p></div>
+      </div>
+      <div class="swiper-button-prev"></div><!--左箭头。如果放置在swiper-container外面，需要自定义样式。-->
+      <div class="swiper-button-next"></div><!--右箭头。如果放置在swiper-container外面，需要自定义样式。-->
+      <div class="swiper-pagination"></div><!--分页器。如果放置在swiper-container外面，需要自定义样式。-->
+    </div>
+    <h5></h5>
   </div>
-</div>
-
+  <footer>
+    <div>
+      <svg t="1607582589250" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7153" width="26" height="26"><path d="M556.586667 159.36l288.490666 183.914667A64 64 0 0 1 874.666667 397.248v392.746667a64 64 0 0 1-64 64H555.456l0.021333-196.992H490.666667v196.992H234.666667a64 64 0 0 1-64-64v-398.293334a64 64 0 0 1 30.272-54.4l287.530666-178.346666a64 64 0 0 1 68.138667 0.426666zM810.666667 790.016V397.226667L522.197333 213.333333 234.666667 391.68v398.336h192v-197.013333h192.810666v196.992H810.666667z" p-id="7154" fill="#ffffff"></path></svg>
+      <p>首页</p>
+    </div>
+    <div>
+      <svg t="1607582821822" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3107" width="26" height="26"><path d="M882.8 363.3c13.7 0 25.3 4.3 34.9 13 9.5 8.6 14.3 19.2 14.3 31.8V672c0 11.9-4.8 22.2-14.3 30.9-9.5 8.6-21.2 13-34.9 13h-16.1c-13.7 0-25.3-4.3-34.9-13-9.5-8.6-14.3-18.9-14.3-30.9V408c0-12.5 4.8-23.1 14.3-31.8 9.5-8.6 21.2-13 34.9-13h16.1v0.1z m-725.5 0c13.7 0 25 4.3 34 13 8.9 8.6 13.4 19.2 13.4 31.8V672c0 11.9-4.5 22.2-13.4 30.9-8.9 8.6-20.3 13-34 13h-16.1c-13.7 0-25.3-4.3-34.9-13C96.8 694.3 92 684 92 672V408c0-12.5 4.8-23.1 14.3-31.8 9.5-8.6 21.2-13 34.9-13h16.1v0.1z m493.8-207.6c10.7 4.8 21.9 11.3 33.5 19.7 11.6 8.3 22.8 18.2 33.5 29.5s20.1 23.7 28.2 37.1c8.1 13.4 13.9 27.6 17.4 42.5h-508c9.5-31.6 24.6-57.6 45.2-77.8s41.3-36.4 62.2-48.3l-45.6-71.6c-1.2-1.2-1.3-3.4-0.4-6.7 0.9-3.3 4-6.7 9.4-10.3 4.8-4.2 9.2-6.1 13.4-5.8 4.2 0.3 6.9 1 8.1 2.2l46.5 73.4c16.7-7.8 34.3-13.7 52.8-17.9 18.5-4.2 37.6-6.3 57.3-6.3 40.6 0 78.4 8.4 113.6 25l47.4-74.3c1.2-1.2 3.4-1.8 6.7-1.8 3.3 0 8.2 1.8 14.8 5.4 6 3 9.4 5.7 10.3 8.1 0.9 2.4 0.7 4.2-0.4 5.4l-45.9 72.5z m-249.6 77c7.8 0 14.3-2.7 19.7-8.1 5.4-5.4 8.1-11.9 8.1-19.7 0-7.8-2.7-14.3-8.1-19.7-5.4-5.4-11.9-8.1-19.7-8.1-7.8 0-14.3 2.7-19.7 8.1-5.4 5.4-8.1 11.9-8.1 19.7 0 7.8 2.7 14.3 8.1 19.7 5.4 5.4 12 8.1 19.7 8.1z m220.1-3.6c7.8 0 14.3-2.7 19.7-8.1 5.4-5.4 8.1-11.9 8.1-19.7 0-7.8-2.7-14.3-8.1-19.7-5.4-5.4-11.9-8.1-19.7-8.1-7.8 0-14.3 2.7-19.7 8.1-5.4 5.4-8.1 11.9-8.1 19.7 0 7.8 2.7 14.3 8.1 19.7 5.4 5.4 11.9 8.1 19.7 8.1z m142.2 111.8l0.9 402.6c0 13.1-4.3 24.2-13 33.1S732.5 790 720 790h-12.5v125.2c0 11.9-4.3 22.4-13 31.3-8.6 8.9-19.2 13.4-31.8 13.4h-25.9c-11.9 0-22.2-4.5-30.9-13.4-8.6-8.9-13-19.4-13-31.3V790H428.4v125.2c0 11.9-4.3 22.4-13 31.3-8.6 8.9-19.2 13.4-31.8 13.4h-25.9c-11.9 0-22.2-4.5-30.9-13.4-8.6-8.9-13-19.4-13-31.3V790H304c-12.5 0-23.1-4.5-31.8-13.4-8.6-8.9-13-20-13-33.1V340.9h504.6z" fill="#ffffff" p-id="3108"></path></svg>
+      <p>安卓</p>
+    </div>
+    <div>
+      <svg t="1607582921949" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4107" width="26" height="26"><path d="M649.785306 161.506856C707.566241 88.169516 694.232179 8.165145 689.787492 1.498114c-6.667031-6.667031-93.338433 6.667031-153.341711 86.671402-53.336247 73.33734-53.336247 153.341711-46.669217 160.008742C496.443594 254.845289 583.114997 248.178259 649.785306 161.506856L649.785306 161.506856zM649.785306 161.506856M794.237643 534.860588c-6.667031-106.672495 93.338433-173.342804 113.339526-186.676866l0-6.667031c0 0-86.671402-106.672495-213.34499-100.005464-80.004371 6.667031-120.006557 46.669217-173.342804 46.669217-66.67031 0-126.673588-46.669217-200.010928-46.669217-60.003278 0-233.346082 53.336247-246.680145 280.015299C63.086584 748.205579 223.095326 959.328225 283.098605 999.330411c60.003278 40.002186 100.005464 26.668124 166.675774-6.667031 33.335154-20.001093 140.00765-26.668124 200.010928 13.334062 73.33734 26.668124 180.009835 6.667031 300.016392-260.014206C934.245292 748.205579 800.904674 714.870423 794.237643 534.860588L794.237643 534.860588zM794.237643 534.860588" p-id="4108" fill="#ffffff"></path></svg>
+      <p>苹果</p>
+    </div>
+  </footer>
+  </div>
 </template>
 
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"> </script>
 <script>
 export default {
   name: 'v_header',
@@ -32,11 +65,104 @@ export default {
 components: {
 },
   mounted () {
-
+    var mySwiper = new Swiper('.swiper-container',{
+      effect : 'cube',
+      loop : true,
+      grabCursor : true,
+      autoplay: {
+          delay: 3000,
+          disableOnInteraction: true,
+          stopOnLastSlide: true,
+        },
+      cubeEffect: {
+        slideShadows: true,
+        shadow: true,
+        shadowOffset: 100,
+        shadowScale: 0.6
+      },
+      pagination: {
+        el: '.swiper-pagination',
+          type : 'progressbar',
+          progressbarOpposite: false,
+      },
+    })
 
   }}
 </script>
 
 <style scoped>
-
+    *{
+      margin: 0;
+      padding: 0;
+    }
+.content{
+  width: 100%;
+}
+.swiper-container{
+  width:80%;
+  margin-top: 1rem;
+}
+.swiper-slide video{
+  width: 100%;
+}
+.swiper-slide h5{
+  font-size: 1rem;
+  padding: 1rem 0;
+}
+.model_title{
+  text-align: center;
+  padding: 2rem 0 0;
+  color: #077ae2;
+}
+.swiper-container{
+    --swiper-theme-color: #ff6600;/* 设置Swiper风格 */
+    --swiper-navigation-color: #ccc;/* 单独设置按钮颜色 */
+    --swiper-navigation-size: 30px;/* 设置按钮大小 */
+  }
+  .swiper-button-prev{
+    left: -10%;
+  }
+  .swiper-button-next{
+    right: -10%;
+  }
+  .swiper-container-horizontal>.swiper-pagination-progressbar, .swiper-container-vertical>.swiper-pagination-progressbar.swiper-pagination-progressbar-opposite{
+    transform: translateY(-3px);
+  }  
+/* ============================================= */
+header{
+  width: auto;
+  height: 3rem;
+  background-color: #077ae2;
+  display: flex;
+  align-content: center;
+  padding: 0 1rem;
+  justify-content: space-between;
+}
+.logo{
+  display: flex;
+  align-items: center;
+}
+.menu{
+  display: flex;
+  align-items: center;
+}
+.title{
+  color: #fff;
+  font-size: 1rem;
+  line-height: 3rem;
+}
+footer{
+  width: 100%;
+  margin-top: 10rem;
+  height: 4rem;
+  background-color: #077ae2;
+  display: flex;
+  align-content: center;
+  justify-content:space-around;
+  color: #fff;
+  font-size: 0.8rem;
+}
+footer svg{
+  margin-top: 0.5rem;
+}
 </style>
